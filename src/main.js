f2ae00402f59b3fb834c3da7517f5ae87920f029
Jiwon Han and Steven Fleming <jiwon@>
2020-02-12 13:24:05 -0800

    Sets up webpage that handles API requests and returns the temp and humidity of a user inputted city

--- src/main.js
+++ src/main.js
@@ -2,3 +2,23 @@ import $ from 'jquery';
 import 'bootstrap' ; 
 import 'bootstrap/dist/css/bootstrap.min.css' ; 
 import './styles.css' ;
+import { WeatherService } from './weather-service';
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    const city = $('#location').val();
+    $('#location').val("");
+
+    (async () => {
+      let weatherService = new WeatherService();
+      const response = await weatherService.getWeatherByCity(city);
+      getElements(response);
+    
+    }) (); 
+    
+    function getElements (response) {
+      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+      $('.showTemp').text(`The temperature in ${city} is ${response.main.temp} degrees.`);
+    }
+  });
+});
